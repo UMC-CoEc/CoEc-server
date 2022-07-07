812e845afb0732387a56bc82ab4b2cd298f2ec92
정민석(***8***041) <alstjr3060@kau.kr>
2022-07-08 00:35:52 +0900

    initial commit / 초기세팅

---
+++ src/main/java/com/umc/coec/CoEcApplication.java
@@ -0,0 +1,13 @@
+package com.umc.coec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoEcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoEcApplication.class, args);
+    }
+
+}
